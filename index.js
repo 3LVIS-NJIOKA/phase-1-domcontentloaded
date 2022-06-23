1523e6177af8cdac822d240b8236004500082e20
Elvis Njioka <elvis.njioka@student.moringaschool.com>
2022-06-23 09:41:38 +0300

    save changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+// Your code goes here@@ -1,9 +1,9 @@
+function domContentLoaded() {
+    document.addEventListener("DOMContentLoaded", function(event) {
+      updateDOM()
+    });
+  }
+  document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
+
